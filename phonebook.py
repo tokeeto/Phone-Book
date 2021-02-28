8bf8424f7204e1e967a22d542ecfcabae6bd3cf6
tokeeto <toke.ivo@gmail.com>
2021-02-28 12:55:03 +0000

    Update phonebook.py
    Made some changes for clarity, and a better code flow.

--- phonebook.py
+++ phonebook.py
@@ -10,64 +10,45 @@ class Phonebook:
 
         try:
             self.cursor.execute("CREATE TABLE Phonebook (Name VARCHAR(30) NOT NULL, Telephone VARCHAR(25) PRIMARY KEY, City VARCHAR(20))")
-        except:
+        except:  # bad! Always be explicit about which exceptions you can handle. What if you had a ConnectionError exception?
             pass
 
     def mainMenu(self):
         while True:
-            opt = input("\n1. ADD CONTACT\t\t4. SEARCH SPECIFIC CONTACT\n2. MODIFY CONTACT\t5. EXIT\n3. DELETE CONTACT\n\nSelect: ")
+            opt = -1
+            while opt not in ('1', '2', '3', '4', '5'):
+                opt = input("\n1. ADD CONTACT\t\t4. SEARCH SPECIFIC CONTACT\n2. MODIFY CONTACT\t5. EXIT\n3. DELETE CONTACT\n\nSelect: ")
+                if opt not in ('1', '2', '3', '4', '5'):
+                    print('INVALID INPUT')
 
             if opt == "1":
                 self.addContact()
-                break
-            elif opt == "2":
+            if opt == "2":
                 self.modifyContact()
-                break
-            elif opt == "3":
+            if opt == "3":
                 self.deleteContact()
-                break
-            elif opt == "4":
-                self.searchContact()
-                break
-            elif opt == "5":
-                break
-            else:
-                print("\nINVALID INPUT!!")
-
+            if opt == "4":
+               self.searchContact()
+            if opt == "5":
+                return
+        
     def addContact(self):
         print("\n\n\t\tADD CONTACT")
 
-        contact_name = input("\n\nEnter the name of contact: ").strip().title()
-        if contact_name == "":
-            contact_name = None
-
-        tel_num = input("Enter the telephone number: ").strip()
-        if tel_num == "":
-            tel_num = None
-
-        city = input("Enter the city(Optional): ").strip()
-        if city == "":
-            city = 'None'
-
+		print('\n\n')
+        contact_name = input("Enter the name of contact: ").strip().title() or None
+        tel_num = input("Enter the telephone number: ").strip() or None
+        city = input("Enter the city(Optional): ").strip() or None
+        
         try:
             self.cursor.execute(f"INSERT INTO Phonebook VALUES('{contact_name}', '{tel_num}', '{city}')")
-
         except mysql.connector.errors.IntegrityError:
             print("\nErrors occurred! The 'Name' OR 'Telephone number' cannot be empty AND the 'Telephone number' must be unique!")
             self.addContact()
-
         else:
             print("\nAdding the contact...")
-            sleep(2)
-
             self.db.commit()
             print("\nThe contact number has successfully been added!")
-
-            opt = input("\nEnter '1' to Add another contact, and enter 'any key' to go to main menu: ").strip()
-            if opt == "1":
-                self.addContact()
-            else:
-                self.mainMenu()
 
     def modifyContact(self):
         self.cursor.execute("SELECT Name FROM Phonebook")
@@ -77,62 +58,32 @@ class Phonebook:
         copy_name = contact_name,    # convert the string into tuple
 
         print("\nSearching...")
-        sleep(2)
 
+        # bad sql code - names could be identical
         if copy_name in name_list:
             new_num = input("Enter the new telephone number: ").strip()
 
             self.cursor.execute(f"UPDATE Phonebook SET Telephone = '{new_num}' WHERE Name = '{contact_name}'")
             self.db.commit()
-
-            print("\nModifying...")
-            sleep(2)
-            print("\nSuccessfully modified!")
+            print("Successfully modified!")
         else:
             print(f"\n{contact_name} does not exist within the Phonebook. Please refer to 'ADD CONTACT'!")
-        
-        opt = input("\nEnter '1' to Modify another contact, and enter 'any key' to go to main menu: ").strip()
-        if opt == "1":
-            self.modifyContact()
-        else:
-            self.mainMenu()
     
     def deleteContact(self):
         contact_name = input("\nEnter the contact name: ").strip()
-
         self.cursor.execute(f"DELETE FROM Phonebook WHERE Name = '{contact_name}'")
         db.commit()
-
-        print("\nDeleting...")
-        sleep(2)
         print("\nDeletion of contact is successful!")
-
-        opt = input("\nEnter '1' to Delete another contact, and enter 'any key' to go to main menu: ").strip()
-        if opt == "1":
-            self.deleteContact()
-        else:
-            self.mainMenu()
-    
+        
     def searchContact(self):
         contact_name = input("\nEnter the contact name to search: ").strip()
-
-        print("\nSearching...")
-        sleep(2)
-
         self.cursor.execute(f"SELECT * FROM Phonebook WHERE Name LIKE '%{contact_name}%'")
         contact_list = self.cursor.fetchall()
 
-        if len(contact_list) == 0:
+        for i in range(len(contact_list)):
+            print(f"\nContact Name:\t\t{contact_list[i][0]}\nTelephone Number:\t{contact_list[i][1]}\nCity:\t\t\t{contact_list[i][2]}")
+        else:
             print("\nThe searched contact is not found!")
-        else:
-            for i in range(len(contact_list)):
-                print(f"\nContact Name:\t\t{contact_list[i][0]}\nTelephone Number:\t{contact_list[i][1]}\nCity:\t\t\t{contact_list[i][2]}")
-
-        opt = input("\nEnter '1' to Search another contact, and enter 'any key' to go to main menu: ").strip()
-        if opt == "1":
-            self.searchContact()
-        else:
-            self.mainMenu()
 
 if __name__ == "__main__":
     load_dotenv()
@@ -146,3 +97,4 @@ if __name__ == "__main__":
 
     my_phonebook = Phonebook(db)
     my_phonebook.mainMenu()
+        
